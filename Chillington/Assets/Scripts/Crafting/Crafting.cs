4f1479d3254b5cbbc0fb8b295e645d1c199606ae
K1NGZT3R <dexg109@gmail.com>
2023-09-18 20:22:45 +0930

    Crafting basic done

--- Chillington/Assets/Scripts/Crafting/Crafting.cs
+++ Chillington/Assets/Scripts/Crafting/Crafting.cs
@@ -19,14 +19,68 @@ public class Crafting : MonoBehaviour
     //Other
     public int landmine;
     public int electricFence;
-
+    public int fence;
+    public int gate;
 
 
     public void CraftHammer()
     {
-        if (wood == 2 && stone == 1)
+        if (wood >= 2 && stone >= 1)
         {
-
+            hammer = hammer + 1;
+            wood = wood - 2;
+            stone = stone - 1;
         }
     }
+
+    public void CraftMachete()
+    {
+        if (metal >= 2 && wood >= 1)
+        {
+            machete = machete + 1;
+            metal = metal - 2;
+            wood = wood - 1;
+        }
+    }
+
+    public void CraftLandmine()
+    {
+        if (metal >= 5 && stone >= 10 && wood >= 2)
+        {
+            landmine = landmine + 1;
+            metal = metal - 5;
+            stone = stone - 10;
+            wood = wood - 2;
+        }
+    }
+
+    public void CraftElectricFence()
+    {
+        if (carBattery >= 1 && metal >= 10)
+        {
+            electricFence = electricFence + 1;
+            carBattery = carBattery - 1;
+            metal = metal - 10;
+        }
+    }
+
+    public void CraftFence()
+    {
+        if (metal >= 10)
+        {
+            fence = fence + 1;
+            metal = metal - 10;
+        }
+    }
+
+    public void CraftGate()
+    {
+        if (metal >= 10 && stone >= 2)
+        {
+            gate = gate + 1;
+            metal = metal - 10;
+            stone = stone - 2;
+        }
+    }
+
 }
