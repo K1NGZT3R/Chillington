659b85074f16bc3c5a4b14205a484ec3908d8d92
AIE\s213165 <deguetct@gmail.com>
2023-09-20 18:41:42 +0930

    Betterer Aim

--- Chillington/Assets/Scripts/Player Scripts/MouseLook.cs
+++ Chillington/Assets/Scripts/Player Scripts/MouseLook.cs
@@ -10,6 +10,14 @@ public class MouseLook : MonoBehaviour
 
     float xRotation = 0f;
 
+    public Camera cam;
+
+    public float defaultFOV = 60;
+    public float aimFOV = 40;
+    public float unAimFOV = 60f;
+    public float lerpTime = 3f;
+    //might need to add the value to variable in unity UI
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -25,7 +33,21 @@ public class MouseLook : MonoBehaviour
 
         playerBody.Rotate(Vector3.up * mouseX);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        if (Input.GetMouseButton(1))
+        {
+            AimDownSight();
+        }
+        else
+        {
+            defaultFOV = Mathf.Lerp(defaultFOV, unAimFOV, lerpTime);
+            cam.fieldOfView = defaultFOV;
+        }
     }
 
-    //test
+    private void AimDownSight()
+    {
+        defaultFOV = Mathf.Lerp(defaultFOV, aimFOV, lerpTime);
+        cam.fieldOfView = defaultFOV;
+    }
 }
