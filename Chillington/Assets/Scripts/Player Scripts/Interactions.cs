d3ff80fcbe8c006b3f468d9619ce7af81640e0e5
K1NGZT3R <dexg109@gmail.com>
2023-09-20 20:22:07 +0930

    Pickups started

--- Chillington/Assets/Scripts/Player Scripts/Interactions.cs
+++ Chillington/Assets/Scripts/Player Scripts/Interactions.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
+    [Header("Interaction layers")]
+    public LayerMask Stone;
+    public LayerMask Wood;
+    public LayerMask Metal;
+    public LayerMask CarBattery;
+    public float PickUpRange;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +20,28 @@ public class Interactions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Stone) && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("hit bit");
+        } 
         
+        if (Physics.Raycast(ray, out RaycastHit hit, Wood) && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("hit bit");
+        }
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Metal) && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("hit bit");
+        } 
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, CarBattery) && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("hit bit");
+        }
+
+
     }
 }
