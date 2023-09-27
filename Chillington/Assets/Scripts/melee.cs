59a96bab4ad13d3a97d361b60665460419291a95
AIE\s213165 <deguetct@gmail.com>
2023-09-27 20:20:36 +0930

    melee

---
+++ Chillington/Assets/Scripts/melee.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class melee : MonoBehaviour
+{
+    public Collider hitRange;
+    public float counting;
+
+    void Start()
+    {
+        hitRange.enabled = false;
+
+    }
+
+
+    void Update()
+    {
+        hits();
+
+    }
+
+    private void hits()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hitRange.enabled = true;
+            Debug.Log("stab");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            hitRange.enabled = false;
+            Debug.Log("no stab");
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hittable"))
+        {
+            Debug.Log("you stabbed a minority");
+        }
+    }
+
+
+
+
+}
